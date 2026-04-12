190fe00a7a871d86441263e88c907f572ed2e088
Abhijeet Manhas <abhijeetmanhas@yahoo.com>
2026-04-12 14:55:58 +0100

    Update API URL to Render backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,14 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5001/api" });
+const API = axios.create({
+  baseURL: "https://wine-chain-backend.onrender.com",
+});
 
 export const getBottles = (params) => API.get("/bottles", { params });
 export const getBottle = (id) => API.get(`/bottles/${id}`);
 export const addBottle = (data) => API.post("/bottles", data);
-export const transferBottle = (id, data) => API.put(`/bottles/${id}/transfer`, data);
+export const transferBottle = (id, data) =>
+  API.put(`/bottles/${id}/transfer`, data);
 export const verifyBottle = (id) => API.get(`/bottles/${id}/verify`);
 export const deleteBottle = (id) => API.delete(`/bottles/${id}`);
 
